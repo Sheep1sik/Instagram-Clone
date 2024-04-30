9d08c6a2ebe6afc6d99b58a1c2bfbcd8bf98bf5e
Sheep1sik <bohg6799@naver.com>
2024-04-30 14:05:57 +0900

    Feat: Project Setup

---
+++ InstagramTutorial/InstagramTutorial/App/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by 양원식 on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
