fa518568e37d6b3620dacebfa8a38a0178bb6d82
Sheep1sik <bohg6799@naver.com>
2024-04-30 18:28:15 +0900

    Feat: Explore View 구현 - SearchView

--- InstagramTutorial/InstagramTutorial/Core/Search/SearchView.swift
+++ InstagramTutorial/InstagramTutorial/Core/Search/SearchView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    HStack {
+                        Text("최근 검색")
+                            .fontWeight(.semibold)
+                        Spacer()
+                    } //: HSTACK
+                    .padding(.horizontal)
+                    .padding(.bottom, 10)
+                    ForEach(0 ... 15, id: \.self) { user in
+                        HStack {
+                            Image("User_b2_iiin")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                            .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                Text("b2_iiin")
+                                    .fontWeight(.semibold)
+                                
+                                Text("서빈 • sheep1sik님 외 4명이 팔로우합니다")
+                                    .foregroundColor(.gray)
+                            } //: VSTACK
+                            .font(.footnote)
+                            
+                            Spacer()
+                        } //: HSTACK
+                        .padding(.horizontal)
+                    } //: LOOP
+                } //: LAZY
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search...")
+            } //: SCROLL
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        } //: NAVIGATION
     }
 }
 
